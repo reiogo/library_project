62d51a7e7950973e9e1cc1fe41c6e37b0c15d3ee
reiogo <raogoda2@gmail.com>
2024-07-05 23:38:59 +0900

    Add submit form
    Making the book submission form work with javascript

--- javascript.js
+++ javascript.js
@@ -10,21 +10,25 @@ function Book(title, author, pages, readOrNot) {
 }
 
 function addBookToLibrary(title, author, pages, readOrNot) {
- let book = new Book(title, author, pages, readOrNot);
+
+  let book = new Book(title, author, pages, readOrNot);
   myLibrary.push(book)
+
 }
 
 addBookToLibrary("Harry Potter", "JK Rowling", 700, "Yes");
-displayBooks();
+
+
 
 function displayBooks() {
 
   const booksContainer = document.querySelector(".books-container");
   let card = document.createElement("div");
-  booksContainer.appendChild(card);
+  booksContainer.replaceChild(card, booksContainer.childNodes[0]);
   
 
   for (let i=0; i < myLibrary.length; i++) {
+    
     let titleCard = document.createElement ("span");
     titleCard.innerText += `Title: ${myLibrary[i].title}`;
     card.appendChild(titleCard)
@@ -43,7 +47,7 @@ function displayBooks() {
 // Display form upon button click
 document.querySelector(".new-book-button").addEventListener("click", displayForm);
 
-function displayForm () {
+function displayForm() {
 
   let submitForm = document.querySelector(".submit-form");
   let submitButton = document.querySelector(".new-book-button");
@@ -58,9 +62,33 @@ function displayForm () {
   //   submitButton.style.display = "block";
   // }
 }
+
+
+
+// Submit new book 
+
+const bookSubmitForm = document.querySelector(".submit-button");
+
+bookSubmitForm.addEventListener("click", bookSubmit);
+
+
+function bookSubmit(event) {
+
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const readOrNot = document.querySelector("input[type=radio]:checked").value;
+  
+ 
+  addBookToLibrary(title, author, pages, readOrNot);
+  displayBooks();
+}
+
   
   
 
-  
 
-
+// Display books
+displayBooks();
