13a55a94d142ae10662333c77f84e3122f6d01ba
reiogo <raogoda2@gmail.com>
2024-07-05 10:00:23 +0900

    Add sample book to page

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 
+
 function Book(title, author, pages, readOrNot) {
   // the constructor
   this.title = title;
@@ -9,7 +10,36 @@ function Book(title, author, pages, readOrNot) {
 }
 
 function addBookToLibrary(title, author, pages, readOrNot) {
-  `book_${myLibrary.indexOf(-1) + 1}`= Book(title, author, pages, readOrNot)
-   
+ let book = new Book(title, author, pages, readOrNot);
+  myLibrary.push(book)
 }
+
+addBookToLibrary("Harry Potter", "JK Rowling", 700, "Yes");
+displayBooks();
+
+function displayBooks() {
+
+  const booksContainer = document.querySelector(".books-container");
+  let card = document.createElement("div");
+  booksContainer.appendChild(card);
+  
+
+  for (let i=0; i < myLibrary.length; i++) {
+    let titleCard = document.createElement ("span");
+    titleCard.innerText += `Title: ${myLibrary[i].title}`;
+    card.appendChild(titleCard)
+    let authorCard = document.createElement ("span");
+    authorCard.innerText += `Author: ${ myLibrary[i].author }`;
+    card.appendChild(authorCard)
+    let pagesCard = document.createElement ("span");
+    pagesCard.innerText += `Pages: ${ myLibrary[i].pages }`;
+    card.appendChild(pagesCard)
+    let readCard = document.createElement ("span");
+    readCard.innerText += `Have you read this?: ${ myLibrary[i].readOrNot }`;
+    card.appendChild(readCard)
+  }
+
+
+}
+
 
